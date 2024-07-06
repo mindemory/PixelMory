6e5f4f63098bcdbf46ffdfbb41ee93a43b679ed5
Mrugank Dake <mrugankdake@gmail.com>
2024-07-06 15:39:44 -0400

    function is ready to go, works best when the image does not have black art in between

---
+++ rembackground.py
@@ -0,0 +1,27 @@
+import os
+import numpy as np
+from PIL import Image
+import argparse
+
+def main(fname):
+    # file_name = 'Fig1.png'
+    img = Image.open(fname)
+    img_array = np.array(img)
+
+    img_new_array = img_array.copy()
+    # Add an empty column for alpha values
+    img_new_array = np.insert(img_new_array, 3, 255, axis=2)
+
+    # Set the alpha value to 0 for the white pixels
+    img_new_array[img_new_array[:,:,0] == 255] = [255, 255, 255, 0]
+    # Change the black pixels to be white but not transparent
+    img_new_array[img_new_array[:,:,0] == 0] = [255, 255, 255, 255]
+    # Save the image
+    img_new = Image.fromarray(img_new_array)
+    img_new.save(fname[:-4] + '_transparent.png', 'PNG', quality=300)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('fname', type=str, help='File name')
+    args = parser.parse_args()
+    main(args.fname)
